c997cb7edc39151d99df57cc37e88491efcba4b0
Stef97 <sfuscon@live.it>
2019-05-01 12:03:59 +0200

    New version of BitMap_get, started BitMap_set and after try some test

--- bitmap.c
+++ bitmap.c
@@ -1,32 +1,33 @@
-#include <math.h>
 #include "bitmap.h"
+#define BITS 8
 
 BitMapEntryKey BitMap_blockToIndex(int num){
     
     BitMapEntryKey bmapentry;
-    bmapentry.entry_num = num / 8;   //how many bytes?
-    bmapentry.bit_num = num - (8 * (num / 8));   //offset
+    bmapentry.entry_num = num / BITS;   //quanti bytes?
+    bmapentry.bit_num = (char)(num - ((num / BITS) * BITS));   //offset
     return bmapentry;
 }
 
 int BitMap_indexToBlock(int entry, uint8_t bit_num){
     
     if(entry > 0 && bit_num > 0)
-        return entry * 8 + bit_num;
+        return entry * BITS + bit_num;
     else
         return -1; //valori passati alla funzioni errati perchè negativi    
 }
 
 int BitMap_get(BitMap* bmap, int start, int status){
 
-    if(start > (bmap -> num_bits))
-        return -1;    //non avrò bit disponibili a partire da quel start
+    if(start > (bmap -> num_bits) || start < 0 || status < 0)
+        return -1;    //non avrò bit disponibili a partire da quel start, oppure i valori passati non sono corenti
 
     BitMapEntryKey bmapentry;
     int bit;
     while(start < (bmap -> num_bits)){
         bmapentry = BitMap_blockToIndex(start);
         //da inserire il valore del bit, ipotesi shift verso destra di bit_num...
+        bit = bmap -> entries[bmapentry.entry_num] >> (bmapentry.bit_num) & 0x01;   //Brevemente: ho un array di char, prendo la posizione dell'entry all'interno di quest'ultimo, shifto dell'offset e grazie alla maschera ottengo 0 o 1 a seconda del bit
         if(bit == status)
             return start;
         
@@ -35,6 +36,12 @@ int BitMap_get(BitMap* bmap, int start, int status){
     
     return -1;
 }
+
+int BitMap_set(BitMap* bmap, int pos, int status){
+    
+}
+
+
 
 
 
