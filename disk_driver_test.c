27fcaafdb54962855648efa8660a60d25baf0e13
Stef97 <sfuscon@live.it>
2019-05-04 17:43:38 +0200

    Inserted code to test, init must be fixed...

--- disk_driver_test.c
+++ disk_driver_test.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "disk_driver.h"
 
 int main(int argc, char* argv[]){
+    
+    DiskDriver* diskdriver = (DiskDriver*)malloc(sizeof(DiskDriver));
+    const char* filename = "./disk.txt";
+
+    DiskDriver_init(diskdriver, filename, 8);
+
+    free(diskdriver);
+
     return 0;
 }
