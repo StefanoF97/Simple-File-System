fe2715f5786b7f5cbc21b66f1705cfb3a5a2301e
Stef97 <sfuscon@live.it>
2019-05-07 12:17:06 +0200

    Tryed function to write in disk, but i found an error in bitmap_entries, to fix...

--- disk_driver_test.c
+++ disk_driver_test.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "disk_driver.h"
+#include "simplefs.h"
 
 int main(int argc, char* argv[]){
     
     DiskDriver* diskdriver = (DiskDriver*)malloc(sizeof(DiskDriver));
     const char* filename = "./disk.txt";
 
-    DiskDriver_init(diskdriver, filename, 16);
+    DiskDriver_init(diskdriver, filename, 3);
 
     printf("bitmap_blocks: %d\n", diskdriver ->header ->bitmap_blocks);
     printf("bitmap_entries: %d\n", diskdriver ->header ->bitmap_entries);
@@ -24,10 +25,44 @@ int main(int argc, char* argv[]){
     printf("\n");
 
     if(!DiskDriver_flush(diskdriver))
-        printf("Flushing with success\n");
+        printf("Flushing with success\n\n\n");
 
     //Started test from here...
 
+    //I'm going to create 3 file block when i can write som data, before i need to alloc the header(in simplefs it will explain the chain...)
+    BlockHeader blockheader;
+    blockheader.next_block = 1;
+    blockheader.previous_block = 1;
+    blockheader.block_in_file = 1;
+
+    FileBlock flb1;
+    flb1.header = blockheader;
+    
+    for(i = 0; i < BLOCK_SIZE - sizeof(BlockHeader); i++)
+        flb1.data[i] = 7;
+    
+    FileBlock flb2;
+    flb2.header = blockheader;
+    for(i = 0; i < BLOCK_SIZE - sizeof(BlockHeader); i++)
+        flb2.data[i] = 1;
+    
+    FileBlock flb3;
+    flb3.header = blockheader;
+    for(i = 0; i < BLOCK_SIZE - sizeof(BlockHeader); i++)
+        flb3.data[i] = 1;
+
+    //Trying to write something on the disk starting from fileblock
+    printf("I'm writing first FileBlock in disk(flb1)\n");
+    if(DiskDriver_writeBlock(diskdriver, &flb1, 0) == -1){
+        printf("Error in writing block 0 of flb1\n");
+        return -1;
+    }
+    DiskDriver_flush(diskdriver);
+    printf("Bitmap in posizione 0: %d\n", diskdriver ->bitmap_data[0]);
+    printf("free_blocks: %d\n", diskdriver ->header ->free_blocks);
+    printf("first_free_block :%d\n", diskdriver ->header ->first_free_block);
+    printf("Not success because bitmap_entries is not 0\n\n\n");
+    
     free(diskdriver);
 
     return 0;
