7903c30dbf908c2eb56c3a5a9ecc93b4fcce0cf1
Stef97 <sfuscon@live.it>
2019-05-11 14:04:04 +0200

    First version of SimpleFS_format

--- simplefs.c
+++ simplefs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "simplefs.h"
 #include "disk_driver.h"
 #include "bitmap.h"
@@ -28,4 +29,30 @@ DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk){
     printf("Init of SimpleFS is a success\n");
 
     return fsb;
+}
+
+void SimpleFS_format(SimpleFS* fs){
+    
+    if(fs == NULL){
+        printf("fs is NULL in SimpleFS_format\n");
+        return;
+    }
+
+    FirstDirectoryBlock fdb = { 0 };  //set to 0 to initialize
+    fdb.fcb.block_in_disk = 0;        //in position 0
+    fdb.fcb.directory_block = -1;     //top level diectory, like a "root", no parent directory
+    fdb.fcb.is_dir = 1;
+    strcpy(fdb.fcb.name, "\0");       //need to use strcpy, without error
+    fdb.header.previous_block = -1;
+    fdb.header.next_block = -1;       //in the future it could be have a next block, now it hasn't
+    fdb.header.block_in_file = 0;     //repeatedly position 0
+    fdb.num_entries = fs ->disk ->header ->bitmap_entries;
+
+    //no need to clear bitmap block because in DiskDriver_init i've just cleared
+
+    if(DiskDriver_writeBlock(fs ->disk, &fdb, 0) == -1){
+        printf("Error in writing disk at position 0 in SimpleFS_format\n");
+        return;
+    }
+    
 }
