20dd82dcb35908579cc417d74814e788110d27f7
Stef97 <sfuscon@live.it>
2019-05-27 16:45:40 +0200

    Writing tests for SimpleFS

--- simplefs_test.c
+++ simplefs_test.c
@@ -1,9 +1,40 @@
 #include "simplefs.h"
+#include "disk_driver.h"
+#include "bitmap.h"
 #include <stdio.h>
-int main(int agc, char** argv) {
-  printf("FirstBlock size %ld\n", sizeof(FirstFileBlock));
-  printf("DataBlock size %ld\n", sizeof(FileBlock));
-  printf("FirstDirectoryBlock size %ld\n", sizeof(FirstDirectoryBlock));
-  printf("DirectoryBlock size %ld\n", sizeof(DirectoryBlock));
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+  
+    printf("FirstBlock size %ld\n", sizeof(FirstFileBlock));
+    printf("DataBlock size %ld\n", sizeof(FileBlock));
+    printf("FirstDirectoryBlock size %ld\n", sizeof(FirstDirectoryBlock));
+    printf("DirectoryBlock size %ld\n\n", sizeof(DirectoryBlock));
+
+    printf("Starting test for SimpleFS....\n\n");
+
+    printf("Tests for SimpleFS_init and SimpleFS_format\n");
+    
+    DiskDriver disk;
+    SimpleFS sfs;
+
+    DiskDriver_init(&disk, "disk.txt", 512);
+    printf("Disk has been created\n");
+
+    DirectoryHandle* dir_root = SimpleFS_init(&sfs, &disk);
+    if(dir_root == NULL){
+        printf("Perhaps i need to format(i need to write something to read)...\n");
+        SimpleFS_format(&sfs);
+        dir_root = SimpleFS_init(&sfs, &disk);
+        if(dir_root == NULL){
+            printf("Problems in init disk");
+            return -1;
+        }
+    }
+
+    
+    
+    return 0;
+
   
 }
