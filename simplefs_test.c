f9f62997b6992a948e5e7d62111e30b1d3801b4c
Stef97 <sfuscon@live.it>
2019-09-16 14:53:16 +0200

    Added some tests

--- simplefs_test.c
+++ simplefs_test.c
@@ -33,28 +33,75 @@ int main(int argc, char** argv) {
     }
 
     printf("TESTS FOR SimpleFS_createFile\n\n");
-    printf("Creo un file chiamato first_file\n");
-    FileHandle* filehandle1 = SimpleFS_createFile(dir_root, "first_file");
-    if(filehandle1 == NULL){
+    
+    printf("Creo un primo file chiamato first_file\n");
+    FileHandle* filehandle = SimpleFS_createFile(dir_root, "first_file");
+    if(filehandle == NULL){
         printf("Error in creating a file(ERROR)\n");
-        exit(1);
     }
     else{
         printf("FileHandle creato correttamente(SUCCESS)\n");
-        printf("Posizione della directory nel file: %d\n\n", filehandle1 ->directory ->header.block_in_file);
-        SimpleFS_close(filehandle1);
+        printf("Nome del file creato: %s\n\n", filehandle ->fcb ->fcb.name);
+        SimpleFS_close(filehandle);
     }
     
-    printf("Creo un altro file chiamato first_file\n");
-    FileHandle* filehandle2 = SimpleFS_createFile(dir_root, "first_file");
-    if(filehandle2 != NULL){
+    printf("Creo un secondo file chiamato first_file\n");
+    filehandle = SimpleFS_createFile(dir_root, "first_file");
+    if(filehandle != NULL){
         printf("FileHandle creato correttamente(ERROR)\n\n");
-        SimpleFS_close(filehandle2);
+        SimpleFS_close(filehandle);
     }
     else{
         printf("FileHandle non creato perchè il file è già esistente(SUCCESS)\n\n");
     }
-    
+
+    printf("Creo un terzo file chiamato second_file\n");
+    filehandle = SimpleFS_createFile(dir_root, "second_file");
+    if(filehandle != NULL){
+        printf("FileHandle creato correttamente(SUCCESS)\n\n");
+        printf("Nome del file creato: %s\n\n", filehandle ->fcb ->fcb.name);
+        SimpleFS_close(filehandle);
+    }
+    else{
+        printf("FileHandle non creato perchè il file è già esistente(ERROR)\n\n");
+    }
+
+    printf("\n\n");
+
+    printf("TESTS FOR SimpleFS_openFile\n\n");
+
+    printf("Provo ad aprie il file 'first_file' nella directory root\n");
+    FileHandle* file_to_open = SimpleFS_openFile(dir_root, "first_file");
+    if(file_to_open == NULL){
+        printf("Error in opening file 'first_file'(ERROR)\n");
+    }
+    else{
+        printf("File 'first_file' aperto (SUCCESS)\n");
+        printf("Nome del file aperto -> %s\n\n", file_to_open ->fcb ->fcb.name);
+        SimpleFS_close(file_to_open);
+    }
+
+    printf("Provo ad aprie il file 'second_file' nella directory root\n");
+    file_to_open = SimpleFS_openFile(dir_root, "second_file");
+    if(file_to_open == NULL){
+        printf("Error in opening file 'second_file'(ERROR)\n");
+    }
+    else{
+        printf("File 'second_file' aperto (SUCCESS)\n");
+        printf("Nome del file aperto -> %s\n\n", file_to_open ->fcb ->fcb.name);
+        SimpleFS_close(file_to_open);
+    }
+
+    printf("Provo ad aprie il file 'fourth_file' nella directory root\n");
+    file_to_open = SimpleFS_openFile(dir_root, "fourth_file");
+    if(file_to_open == NULL){
+        printf("Error in opening file 'fourth_file'(SUCCESS)\n\n");
+    }
+    else{
+        printf("File 'fourth_file' aperto (ERROR)\n");
+        printf("Nome del file aperto -> %s\n\n\n", file_to_open ->fcb ->fcb.name);
+        SimpleFS_close(file_to_open);
+    }
     
     /*
     int res = SimpleFS_mkDir(dir_root, "subDir1");
